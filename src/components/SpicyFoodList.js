76d63ffd9c5b2ba38c5cb44121438855432f4a49
Caroline <bancroftwanjiru10000@gmail.com>
2024-08-05 15:19:36 +0300

    React state and Array

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -5,7 +5,13 @@ function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
   function handleAddFood() {
+    // the varibale newFood stores the array of objects that need to be added to state.
     const newFood = getNewRandomSpicyFood();
+//  for the the new array of objects to be added we need to mutate the existing data in teh varibale in the state.
+// this is done by creating a new array, creaitn a copy of the original array using the spread operator then lsing the new array to be added
+    const newFoodArray= [...foods, newFood];
+    // then passing the mutated array to the setter function.(passing a new value, that will result in React to re-render.)
+    setFoods(newFoodArray);
     console.log(newFood);
   }
 
